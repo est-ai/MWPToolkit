7540d39f066cb6ced6aa0dd66f2cf073a41840bb
Kim Minjong <make.dirty.code@gmail.com>
2021-10-29 00:35:01 +0000

    fix operation and premask

--- mwptoolkit/data/dataset/abstract_dataset.py
+++ mwptoolkit/data/dataset/abstract_dataset.py
@@ -159,31 +159,7 @@ class AbstractDataset(object):
                 it['Question'] = prompting(it, prompt_table, regex)
             for it in self.testset:
                 it['Question'] = prompting(it, prompt_table, regex)
-                
-        if self.pre_mask == MaskSymbol.NUM or self.pre_mask == MaskSymbol.number:
-            for it in self.trainset:
-                question, num_list = num_masking(it['Question'], self.pre_mask, self.mask_grouping)
-                equation = num_masking_const(it['Equation'], self.pre_mask, num_list)
-                
-                it['Question'] = question
-                it['Equation'] = equation
-                it['Numbers'] = ' '.join(num_list)
-            for it in self.validset:
-                question, num_list = num_masking(it['Question'], self.pre_mask, self.mask_grouping)
-                equation = num_masking_const(it['Equation'], self.pre_mask, num_list)
-                
-                it['Question'] = question
-                it['Equation'] = equation
-                it['Numbers'] = ' '.join(num_list)
-            for it in self.testset:
-                question, num_list = num_masking(it['Question'], self.pre_mask, self.mask_grouping)
-                equation = num_masking_const(it['Equation'], self.pre_mask, num_list)
-                
-                it['Question'] = question
-                it['Equation'] = equation
-                it['Numbers'] = ' '.join(num_list)
-        elif self.pre_mask is not None:
-            raise NotImplementedError
+        
 
     def _load_fold_dataset(self):
         """read one fold of dataset from file. 
